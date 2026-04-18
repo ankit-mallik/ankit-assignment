7b4bbe28500f77aaf84739a3f2ad69a92ef60092
ankit-mallik <hritikmallik@gmail.com>
2026-04-18 09:56:19 +0545

    routing to corrent subfolder of backend

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -1,6 +1,11 @@
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.config.settings")
 application = get_wsgi_application()
 app = application
